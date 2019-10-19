7c731ee310e054e61de6fe0a4e0c8e1e45a9ebab
David Ackroyd <davidackroydmusic@gmail.com>
2019-10-19 22:42:45 +0100

    basic enemy

---
+++ TakeThis/Enemy.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+export var pointA :Vector2
+export var pointB :Vector2
+export var speed :int #Number of frames
+
+var direction
+var step
+
+func _ready():
+	global_position = pointA
+	step = (pointB - pointA) / speed
+	direction = 1
+	
+func _process(delta):
+	if direction == 1:
+		if exceeded(global_position, step, pointB):
+			global_position += step
+		else:
+			direction = 0
+	else:
+		if exceeded(global_position, -step, pointA):
+			global_position -= step
+		else:
+			direction = 1
+
+func exceeded(current, s, target):
+	if s.x > 0:
+		if current.x > target.x:
+			return false
+	else:
+		if current.x < target.x:
+			return false
+	if s.y > 0:
+		if current.y > target.y:
+			return false
+	else:
+		if current.y < target.y:
+			return false
+	return true
