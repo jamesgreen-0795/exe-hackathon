9befd4e72b25ea91417d2a64d2e727939da0f5a0
David Ackroyd <davidackroydmusic@gmail.com>
2019-10-19 18:42:02 +0100

    fuck yeah ladies and gents

--- TakeThis/Player.gd
+++ TakeThis/Player.gd
@@ -1,27 +1,29 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-export (int) var speed = 100
-export (int) var maxSpeed = 200
+export (int) var speed = 300
+export (int) var jump_speed = -350
+export (int) var gravity = 1000
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export (float, 0, 1.0) var friction = 0.5
+export (float, 0, 1.0) var acceleration = 0.25
+
+func get_input():
+	var dir = 0
+	if Input.is_action_pressed("ui_right"):
+		dir += 1
+	if Input.is_action_pressed("ui_left"):
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
 
 func _physics_process(delta):
-	update_input(delta)
-	self.angular_velocity = 0.0
-
-func update_input(delta):
-	
-    if Input.is_action_pressed('ui_right') and self.linear_velocity.x < maxSpeed:
-        self.linear_velocity.x += speed * delta
-    if Input.is_action_pressed('ui_left') and self.linear_velocity.x > -maxSpeed :
-        self.linear_velocity.x -= speed * delta
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = jump_speed
