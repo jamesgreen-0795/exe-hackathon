7c865f857aa85be98f58b3e7e995d955757bd606
Matthew Spence <matthew.spence@clock-work.co.uk>
2019-10-20 04:44:59 +0100

    platforms have end option

--- TakeThis/SpriteFloorEnd.gd
+++ TakeThis/SpriteFloorEnd.gd
@@ -1,4 +1,4 @@
-extends AnimationPlayer
+extends Sprite
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -6,7 +6,6 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	play("Floor2")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
